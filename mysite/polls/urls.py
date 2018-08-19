a4c068e0e4f88b1dccba0116156180fd6a039630
gabrielslach <gabdrix09@gmail.com>
2018-08-19 11:07:20 +0800

    First Commit
    Manual upload of files

---
+++ mysite/polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    #polls/
+    path('',views.index, name='index'),
+    #polls/1/
+    path('<int:question_id>/', views.detail, name='detail'),
+    #polls/1/results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
